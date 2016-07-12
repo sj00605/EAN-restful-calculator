3144ea4864f0eed0e2a8b6aca5c4ed98fd0998d3
Samantha Jacobs <Samantha.Jacobs@jhuapl.edu>
2016-07-12 14:03:39 -0400

    karma junit test for controller

--- public/app/calculator_controller_unittests.js
+++ public/app/calculator_controller_unittests.js
@@ -45,6 +45,7 @@ describe('calculator controller tests', function(){
 			$httpBackend.expectGET('/api/operators');
 			var controller = createController();
 			$httpBackend.flush();
+			
 		});
 		
 		
@@ -55,7 +56,6 @@ describe('calculator controller tests', function(){
 			$httpBackend.expectGET('/api/operators');
 			var controller = createController();
 			$httpBackend.flush();
-			console.log($rootScope.result);
 			expect($rootScope.result).toBe('Something went wrong');
 		});
 
@@ -65,22 +65,131 @@ describe('calculator controller tests', function(){
 	describe('http post', function(){
 		
 		it('should pass addition', function() {
-
 			var controller = createController();
 			
+			$rootScope.op = "ADD";
 			$httpBackend
-				.expectPOST('/api/calculate/ADD', {"a": "2","b": "1", "op" : "ADD"})
-				.respond(3.00);
+				.whenPOST('/api/calculate/ADD')
+				.respond(200, 3.00);
 			
 			$rootScope.sendPost();
-			
-			console.log(controller);
-			expect($rootScope.result2).toEqual(3.00);
-			
 			$httpBackend.flush();
-			
+			expect($rootScope.result2).toBe('3.00');
 
 		});
+		
+		it('should fail addition', function(){
+			
+			var controller = createController();
+			
+			$rootScope.op = "ADD";
+			
+			$httpBackend
+				.whenPOST('/api/calculate/ADD')
+				.respond(401, 'Client error ADD');
+			
+			$rootScope.sendPost();
+			$httpBackend.flush();
+			expect($rootScope.result2).toBe('Client error ADD');
+
+		});
+		
+		it('should pass subtraction', function(){
+			
+			var controller = createController();
+			
+			$rootScope.op = "SUB";
+			
+			$httpBackend
+				.whenPOST('/api/calculate/SUB')
+				.respond(200, 1.00);
+			
+			$rootScope.sendPost();
+			$httpBackend.flush();
+			expect($rootScope.result2).toBe('1.00');
+
+		});
+		
+		it('should fail subtraction', function(){
+			
+			var controller = createController();
+			
+			$rootScope.op = "SUB";
+			
+			$httpBackend
+				.whenPOST('/api/calculate/SUB')
+				.respond(401, 'Client error SUB');
+			
+			$rootScope.sendPost();
+			$httpBackend.flush();
+			expect($rootScope.result2).toBe('Client error SUB');
+
+		});
+		
+		it('should pass multiplication', function(){
+			
+			var controller = createController();
+			
+			$rootScope.op = "MUL";
+			
+			$httpBackend
+				.whenPOST('/api/calculate/MUL')
+				.respond(200, 2.00);
+			
+			$rootScope.sendPost();
+			$httpBackend.flush();
+			expect($rootScope.result2).toBe('2.00');
+
+		});
+		
+		it('should fail multiplication', function(){
+			
+			var controller = createController();
+			
+			$rootScope.op = "MUL";
+			
+			$httpBackend
+				.whenPOST('/api/calculate/MUL')
+				.respond(401, 'Client error MUL');
+			
+			$rootScope.sendPost();
+			$httpBackend.flush();
+			expect($rootScope.result2).toBe('Client error MUL');
+
+		});
+		
+		it('should pass division', function(){
+			
+			var controller = createController();
+			
+			$rootScope.op = "DIV";
+			
+			$httpBackend
+				.whenPOST('/api/calculate/DIV')
+				.respond(200, 0.50);
+			
+			$rootScope.sendPost();
+			$httpBackend.flush();
+			expect($rootScope.result2).toBe('0.50');
+
+		});
+		
+		it('should fail division', function(){
+			
+			var controller = createController();
+			
+			$rootScope.op = "DIV";
+			
+			$httpBackend
+				.whenPOST('/api/calculate/DIV')
+				.respond(401, 'Client error DIV');
+			
+			$rootScope.sendPost();
+			$httpBackend.flush();
+			expect($rootScope.result2).toBe('Client error DIV');
+
+		});
+		
 	});
 	
 	
